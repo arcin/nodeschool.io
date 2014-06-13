89dc4900ecf8194c0e8f5a3c5e68f0bd91c3ac50
Charles Arciniega <charlesarcin@gmail.com>
2014-06-12 17:56:41 -0700

    makes use of logger module. logs correct files to screen

--- makeItModular.js
+++ makeItModular.js
@@ -2,3 +2,13 @@ var directory = process.argv[2],
     fileExtension = process.argv[3],
     logger = require('./myModule');
 
+logger(directory, fileExtension, function(err, filtered){
+  if (err) {
+    console.log("there was an error:", err);
+    return;
+  }
+
+  for (var j = 0; j < filtered.length; j++){
+    console.log(filtered[j]);
+  }
+});
