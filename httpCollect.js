41161616dc71226aa897886cddf56f7b4fbb8eda
Charles Arciniega <charlesarcin@gmail.com>
2014-06-12 19:36:32 -0700

    collects data on data emittion. Logs data on end emittion

--- httpCollect.js
+++ httpCollect.js
@@ -3,4 +3,12 @@ var http = require('http'),
 
 http.get(url, function(res){
   res.setEncoding('utf-8');
+  var chars = '';
+  res.on('data', function(data){
+    chars = chars + data;
+  });
+  res.on('end', function(){
+    console.log(chars.length);
+    console.log(chars);
+  });
 });
