fc5d9cb6438e651afc82970f0a19903d1a7d25a3
Charles Arciniega <charlesarcin@gmail.com>
2014-06-12 17:59:28 -0700

    adds notes on callback pattern

--- myModule.js
+++ myModule.js
@@ -6,12 +6,14 @@ function logFiles(dirname, filext, callback){
   fs.readdir(dirname, function(err, files){
     if (err) return callback(err);
 
+    // My module does the necessary work here
     for (var i = 0; i < files.length; i++) {
       if(path.extname(files[i]) === '.'+filext){
         fList.push(files[i]);
       }
     }
 
+    // hand control to the callback, passing in the filtered data
     callback(null, fList);
   });
 }
