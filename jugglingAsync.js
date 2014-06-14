ad0f45b7c1dd0b1fbab6d60cb32162fd5436f71b
Charles Arciniega <charlesarcin@gmail.com>
2014-06-13 20:19:40 -0700

    first refactor

--- jugglingAsync.js
+++ jugglingAsync.js
@@ -1,2 +1,37 @@
 var http = require('http'),
-    urls = process.argv.slice(2, process.argv.length);
+    bl = require('bl'),
+    urlCount = process.argv.length - 2,
+    results = [],
+    count = 0;
+
+var logAllResults = function (results) {
+  for (var i = 0; i < urlCount; i++) {
+    console.log(results[i]);
+  }
+};
+
+var handleRequests = function(requestFunction){
+  for (var i = 0; i < 3; i++) {
+    requestFunction(i);
+  }
+};
+
+var httpGet = function (index) {
+  http.get(process.argv[2 + index], function (stream) {
+    stream.pipe(bl(function (err, content) {
+      if (err) {
+        return console.error(content);
+      }
+
+      results[index] = content.toString();
+      count++;
+
+      if (count > 2) {
+        logAllResults(results);
+      }
+    }));
+  });
+};
+
+
+handleRequests(httpGet);
