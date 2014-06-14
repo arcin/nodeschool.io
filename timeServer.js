a9bdf3bf2ecbb103066b120c096d2290ab5a2bfb
Charles Arciniega <charlesarcin@gmail.com>
2014-06-13 21:17:02 -0700

    modularized code. extracted calback again.

--- timeServer.js
+++ timeServer.js
@@ -1,10 +1,25 @@
 var net = require('net'),
     port = process.argv[2];
 
-var server = net.createServer(socketHandler);
+
+var format = function(time){
+  return time < 10 ? '0' + time : time;
+};
+
+var correctTime = function(){
+  var date = new Date();
+  data = date.getFullYear() + "-"
+       + format(date.getMonth() + 1) + "-"
+       + format(date.getDate()) + " "
+       + format(date.getHours()) + ":"
+       + format(date.getMinutes());
+  return data;
+};
 
 var socketHandler = function(socket){
-
+  socket.end(correctTime() + "\n");
 };
+
+var server = net.createServer(socketHandler);
 
 server.listen(port);
