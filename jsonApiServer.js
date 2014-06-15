5726d03c6bf0d11fc6faf61e1305eb52324f811d
Charles Arciniega <charlesarcin@gmail.com>
2014-06-14 20:58:48 -0700

    creates server and listens on given port

--- jsonApiServer.js
+++ jsonApiServer.js
@@ -1,5 +1,7 @@
 var http = require('http'),
-    url = require('url');
+    url = require('url'),
+    port = process.argv[2];
+
 
 // Serves Json data when receiving a GET request to '/api/parsetime'.
 // request will contain query string with iso key.
@@ -8,3 +10,9 @@ var http = require('http'),
 
 // Second endpoint returns UNIX epoch time in json.
 // ex {"unixtime": 1376136615474}
+
+var server = http.createServer(function(req, res){
+
+});
+
+server.listen(port);
