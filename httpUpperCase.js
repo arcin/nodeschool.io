214089d464bf2b9a63e2904c329fc937fad6f97e
Charles Arciniega <charlesarcin@gmail.com>
2014-06-14 19:16:41 -0700

    only accepts POST requests

--- httpUpperCase.js
+++ httpUpperCase.js
@@ -3,6 +3,9 @@ var http = require('http'),
     port = process.argv[2];
 
 var server = http.createServer(function(req, res){
+  if (req.method != 'POST'){
+    return res.end('Only accepting post requests here.');
+  }
   req.pipe(map(function(chunck){
     return chunck.toString().toUpperCase();
   })).pipe(res);
