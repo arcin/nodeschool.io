6fc23a8886a78d2aa733dfc946d525703449acb2
Charles Arciniega <charlesarcin@gmail.com>
2014-06-14 21:43:43 -0700

    removes pseudo code

--- jsonApiServer.js
+++ jsonApiServer.js
@@ -2,15 +2,6 @@ var http = require('http'),
     url = require('url'),
     port = process.argv[2];
 
-
-// Serves Json data when receiving a GET request to '/api/parsetime'.
-// request will contain query string with iso key.
-
-// The JSON I serve should include only hour, minute, and second properties.
-
-// Second endpoint returns UNIX epoch time in json.
-// ex {"unixtime": 1376136615474}
-
 var parseTime = function(iso){
   var parsedISODate = new Date(iso);
   return {
