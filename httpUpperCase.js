81591a70a382ee6a88ff7aee95e77314c460f40d
Charles Arciniega <charlesarcin@gmail.com>
2014-06-14 19:14:45 -0700

    correctly performs toUpperCase() on stream data

--- httpUpperCase.js
+++ httpUpperCase.js
@@ -2,9 +2,11 @@ var http = require('http'),
     map = require('through2-map'),
     port = process.argv[2];
 
-http.createServer(function(req, res){
+var server = http.createServer(function(req, res){
   req.pipe(map(function(chunck){
-    return chunck.toString().upperCase;
-  }).pipe(res));
+    return chunck.toString().toUpperCase();
+  })).pipe(res);
 });
+
+server.listen(port);
 
