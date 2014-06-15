97e91335b43f1e528298971c17c8e3f326e55ed7
Charles Arciniega <charlesarcin@gmail.com>
2014-06-14 21:39:47 -0700

    abstracts time parsing. need to handle response writeHead

--- jsonApiServer.js
+++ jsonApiServer.js
@@ -11,25 +11,34 @@ var http = require('http'),
 // Second endpoint returns UNIX epoch time in json.
 // ex {"unixtime": 1376136615474}
 
-var server = http.createServer(function(req, res){
-  if (req.method !== 'GET'){
-    return res.end("send me a get request");
-  }
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-
-  var urlProperties = url.parse(req.url, true);
-  var parsedISODate = new Date(urlProperties.query.iso);
-  var unixTime = { unixtime: parsedISODate.getTime() };
-  var parseTime = {
+var parseTime = function(iso){
+  var parsedISODate = new Date(iso);
+  return {
     hour: parsedISODate.getHours(),
     minute: parsedISODate.getMinutes(),
     second: parsedISODate.getSeconds()
   };
+};
+
+var toUnixTime = function(iso){
+  var parsedISODate = new Date(iso);
+  return { unixtime: parsedISODate.getTime() };
+};
+
+
+var server = http.createServer(function(req, res){
+  var urlProperties = url.parse(req.url, true);
+  var iso = urlProperties.query.iso;
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+
+  if (req.method !== 'GET'){
+    return res.end("send me a get request");
+  }
   if (urlProperties.pathname === '/api/parsetime'){
-    res.end(JSON.stringify(parseTime));
+    res.end(JSON.stringify(parseTime(iso)));
   }
   if (urlProperties.pathname === '/api/unixtime'){
-    res.end(JSON.stringify(unixTime));
+    res.end(JSON.stringify(toUnixTime(iso)));
   }
 });
 
