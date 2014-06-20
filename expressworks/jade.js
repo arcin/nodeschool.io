85eb7142a8e55c066424bb75c73dc01a8773c442
Charles Arciniega <charlesarcin@gmail.com>
2014-06-19 20:06:54 -0700

    sets the view engine to jade

--- expressworks/jade.js
+++ expressworks/jade.js
@@ -3,7 +3,7 @@ var express = require('express'),
     port = process.argv[2],
     jadeTemplate = process.argv[3];
 
-
+app.set('view engine', 'jade');
 
 app.get('/home', function(req, res){
   res.render(jadeTemplate, {date: new Date().toDateString()});
