63594249b855d6e01741601ab8e799d6181d988c
Charles Arciniega <charlesarcin@gmail.com>
2014-06-19 20:06:31 -0700

    setting get route to render jade

--- expressworks/jade.js
+++ expressworks/jade.js
@@ -2,3 +2,9 @@ var express = require('express'),
     app = express(),
     port = process.argv[2],
     jadeTemplate = process.argv[3];
+
+
+
+app.get('/home', function(req, res){
+  res.render(jadeTemplate, {date: new Date().toDateString()});
+});
