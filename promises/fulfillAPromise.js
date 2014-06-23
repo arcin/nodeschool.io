8010d7a4aee41f4c7ab14bf8cddf803a89ad13b2
Charles Arciniega <charlesarcin@gmail.com>
2014-06-23 11:07:50 -0700

    fulfills promise and executes console.log

--- promises/fulfillAPromise.js
+++ promises/fulfillAPromise.js
@@ -1,2 +1,8 @@
 var q = require('q'),
     defer = q.defer();
+
+// console.log will be executed when the promise is fulfilled.
+defer.promise.then(console.log);
+
+// here the promise is being fulfilled and "RESOLVED!" is being passed as an argument to console.log.
+setTimeout(defer.resolve, 300, "RESOLVED!");
