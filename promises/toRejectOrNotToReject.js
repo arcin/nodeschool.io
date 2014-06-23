636ba2e1c79ca4051c8043e6d29a89f523cdfd1a
Charles Arciniega <charlesarcin@gmail.com>
2014-06-23 11:49:32 -0700

    adding notes about the permanence of promise values

--- promises/toRejectOrNotToReject.js
+++ promises/toRejectOrNotToReject.js
@@ -2,5 +2,9 @@ var q = require('q'),
     defer = q.defer();
 
 defer.promise.then(console.log, console.log);
+
+// a promise cannot be changed once it has been either resolved or rejected.
+// In this case, "I DID NOT FIRE" will never be logged, because the promise was already
+// resolved.
 defer.resolve('I FIRED');
 defer.reject("I DID NOT FIRE");
