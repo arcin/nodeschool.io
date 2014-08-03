afa80828b486426bdaf38be48fc9bbbc2a5e6b4a
Charles Arciniega <charlesarcin@gmail.com>
2014-08-03 15:58:56 -0700

    nodeschool.io solution. I like the use of an IIFE. Mkaes it easier to pass all 4 arguments (prev, curr, index, and arr) to the function used in reduce

--- functional-javascript-workshop/recursionBasic.js
+++ functional-javascript-workshop/recursionBasic.js
@@ -1,10 +1,8 @@
 function reduce(arr, fn, initial) {
-  var prev = initial,
-      curr = arr[0];
-
-  if (arr.length === 0) return initial;
-  initial = fn(prev, curr);
-  return reduce(arr.slice(1), fn, initial);
+  return (function reduceOne(index, value){
+    if (index > arr.length - 1) return value;
+    return reduceOne(index + 1, fn(value, arr[index], index, arr));
+  })(0, initial);
 }
 
 module.exports = reduce;
